5a84adf92d02d0c1450143e5ccf24269134ddf87
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-03-19 14:58:30 +0100

    modified my models.js

--- models.js
+++ models.js
@@ -3,23 +3,37 @@ const mongoose = require("mongoose");
 let Roomtype = () => {
   // Define the Schema for the roomtype data
   const roomtypeSchema = new mongoose.Schema({
-    name: String,
-  });
+    name: {
+        type: String,
+        required: true
+    }
+  }, {timestamps: true});
 
   // Define roomtype model, to handle our CRUD operations
-  const Roomtype = mongoose.model("Roomtype", roomtypeSchema);
+  return mongoose.model("Roomtype", roomtypeSchema);
 };
 
 let Room = () => {
   // Define the Schema for the room data
   const roomSchema = new mongoose.Schema({
-    name: String,
-    roomType: { type: mongoose.Schema.Types.ObjectId, ref: "Roomtype" },
-    price: Number,
-  });
+    name: {
+        type: String,
+        required: true
+    },
+    roomType:{
+     type: mongoose.Schema.Types.ObjectId, 
+     ref: "Roomtype",
+     required: true
+    },
+    price:{
+        type: Number,
+        required: false,
+        default: null
+    }
+    }, {timestamps: true})
 
   // Define room model, to handle our CRUD operations
-  const Room = mongoose.model("Room", roomSchema);
+  return mongoose.model("Room", roomSchema);
 };
 
 module.exports = { Roomtype, Room };
